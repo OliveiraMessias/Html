fce011f6d8999003eb33e590ee5dc5fe1b89bb76
oliveiramessias <you@example.com>
2025-11-18 20:07:31 -0300

    “commit“

---
+++ Script.js
@@ -0,0 +1,26 @@
+const inputNome = document.getElementById("nome");
+const inputEmail = document.getElementById("email");
+const inputMensagem = document.getElementById("mensagem");
+const botaoEnviar = document.getElementById("btn-enviar");
+const mensagemConfirmacao = document.getElementById("mensagem-confirmacao");
+
+botaoEnviar.addEventListener("click", validarFormulario);
+
+function validarFormulario() {
+  
+    let nome = inputNome.value.trim();
+    let email = inputEmail.value.trim();
+    let mensagem = inputMensagem.value.trim();
+ 
+    if(nome === "" || email === "" || mensagem === ""){
+        alert("Por favor, preencha todos os campos!");
+    } else {
+        alert(`Obrigado ${nome}, sua mensagem foi enviada com sucesso!`);
+
+        mensagemConfirmacao.textContent = `Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`;
+
+        inputNome.value = "";
+        inputEmail.value = "";
+        inputMensagem.value = "";
+    }
+}
